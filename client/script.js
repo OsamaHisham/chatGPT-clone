0fafbaa3931bbebb2cf4100dd7603e8097792aef
Osama Hisham <81423434+OsamaHisham@users.noreply.github.com>
2023-11-16 16:58:20 +0800

    Version 1.0

--- client/script.js
+++ client/script.js
@@ -103,7 +103,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetching the data from server side ~ bot response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-clone-ht89.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
